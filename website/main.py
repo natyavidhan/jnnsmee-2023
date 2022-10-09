50364ccbd6b7173510ee2987848f4bce1a0684da
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-10-10 03:39:38 +0530

    dashboard and stuff

--- website/main.py
+++ website/main.py
@@ -12,23 +12,39 @@ app = Flask(__name__)
 app.config.from_object(dict(os.environ))
 app.secret_key = os.environ.get('SECRET_KEY')
 
+db = MongoClient(os.environ.get('MONGO_URI'))['overspeed']
+admins = db['admins']
+querries = db['querries']
+
+lats = [28.6300, 28.6309, 28.7087, 28.6849, 28.6524, 28.6499, 28.6557, 28.6460, 28.5990, 28.6924]
+lons = [77.2142, 77.2255, 77.1181, 77.2026, 77.1929, 77.2175, 77.2190, 77.2081, 77.1241, 77.1738]
+stations = ["station_1", "station_2", "station_3", "station_4", "station_5", "station_6", "station_7", "station_8", "station_9", "station_10"]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user=session['user'] if 'user' in session else None)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username == os.environ['ADMIN_USERNAME'] and password == os.environ['ADMIN_PASSWORD']:
-            session['logged_in'] = True
+        found = admins.find_one({'_id': username, 'pass': password})
+        if found:
+            session['user'] = found
+            return redirect(url_for('dashboard'))
         return redirect(url_for('index'))
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html', user=session['user'])
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
